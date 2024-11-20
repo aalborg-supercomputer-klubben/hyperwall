516188e73a849132d7ce4f4de17853bf8e4a6ada
Thomas <mast3r@skade.dev>
2024-11-20 22:26:51 +0100

    not quite parallel, but its a lot faster

--- sender.py
+++ sender.py
@@ -13,50 +13,27 @@ RES_X, RES_Y = [int(item) for item in (sys.argv[sys.argv.index("--resolution")+1
 SCREENSHARE = "--screenshare" in sys.argv
 print(f'{X=} {Y=}')
 
-
-manager = mp.Manager()
-class Worker(mp.Process):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.stream = Stream(
-            url = f"rtsp://{REMOTE}/frame/{self.x}/{self.y}",
-            resolution=(int(RES_X/X), int(RES_Y/Y)),
-            fps=60,
-            bitrate="2M"
-        )
-        self.data = manager.list()
-        print(self.stream.url)
-
-        super().__init__(target=self.main_loop)
-
-    def main_loop(self):
-        while True:
-            if len(self.data) == 0:
-                continue
-            frame = self.data.pop(0)
-            x_start = int(len(frame)*(self.x / X))
-            x_end = int(len(frame)*((self.x + 1) / X))
-            y_start = int(len(frame[0])*(self.y / Y))
-            y_end = int(len(frame[0])*((self.y + 1) / Y))
-            final_frame = frame[x_start:x_end, y_start:y_end]
-            self.stream.send(final_frame)
-            break
-
-# main loop
-
-workers:list[Worker] = []
-
-for (x, y) in [(x, y) for x in range(X) for y in range(Y)]:
-    workers.append(Worker(x, y))
-for worker in workers:
-    worker.start()
-
+mapping = {}
 
 cap = cv2.VideoCapture(SOURCE)
 while True:
     _, frame = cap.read()
-    for worker in workers:
-        worker.data.append(frame)
+    for (x, y) in [(x, y) for x in range(X) for y in range(Y)]:
+        if (x, y) not in mapping:
+            mapping[(x, y)] = {
+                "stream":Stream(
+                    url = f"rtsp://{REMOTE}:8554/frame/{x}/{y}",
+                    resolution=(int(RES_X/X), int(RES_Y/Y)),
+                    fps=60,
+                    bitrate="30M"
+                ),
+                "sx":int(len(frame)*(x/X)),
+                "ex":int(len(frame)*((1+x)/X)),
+                "sy":int(len(frame[0])*(y/Y)),
+                "ey":int(len(frame[0])*((1+y)/Y))
+            }
+        m = mapping[(x, y)]
+        final_frame = frame[m["sx"]:m["ex"], m["sy"]:m["ey"]]
+        m["stream"].send(final_frame)
 
-
+    
