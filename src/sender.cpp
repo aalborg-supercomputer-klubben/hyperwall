742b87231555c8d03040b834b6819150c4dd4b92
Thomas <mast3r@skade.dev>
2024-11-21 03:08:01 +0100

    more progress

--- src/sender.cpp
+++ src/sender.cpp
@@ -1,18 +1,32 @@
 #include "sender.hpp"
 
+#include <chrono>
 #include <cstdlib>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/opencv.hpp>
+#include <string>
+#include <thread>
+#include <unordered_map>
 
 // TODO: argument parsing
-auto X = 2;
-auto Y = 2;
+auto X = 1;
+auto Y = 1;
 auto RES_X = 1920;
 auto RES_Y = 1080;
-auto FFMPEG = "";
+auto FRAMERATE = 60;
+auto REMOTE = "localhost:8554";
+
+std::string ffmpeg(int x, int y) {
+    return std::string("ffmpeg -re -f rawvideo -video_size 1920x1080 -pixel_format bgr24 -i pipe: -f rtsp -b:v 2M rtsp://") + REMOTE + "/frame/" + std::to_string(x) + "/" + std::to_string(y);
+}
 
 int main() {
-  FILE* pipeout = popen(FFMPEG, "w");
+  std::unordered_map<int, std::unordered_map<int, FILE*>> pipes;
+  for(auto x = 0; x < X; x++) {
+    for(auto y = 0; y < Y; y++) {
+      pipes[x][y] = popen(ffmpeg(x, y).c_str(), "w");
+    }
+  }
   cv::VideoCapture capture("file.mp4");
   while(true) {
     cv::Mat frame;
@@ -23,10 +37,8 @@ int main() {
         auto ex = frame.cols * ((1.0+x)/X);
         auto sy = frame.rows * (y/Y);
         auto ey = frame.rows * ((1.0+y)/Y);
-        std::cout << "rows: " << frame.rows << " cols: " << frame.cols << std::endl;
-        std::cout << "sx: " << sx << " ex: " << ex << " sy: " << sy << " ey: " << ey << std::endl;
-        auto final_frame = frame(cv::Range(sx, ex), cv::Range(sy, ey));
-        fwrite(frame.data, 1, (size_t)frame.cols * frame.rows, pipeout);
+        auto final_frame = frame(cv::Range(sy, ey), cv::Range(sx, ex));
+        fwrite(final_frame.data, 1, (size_t)final_frame.cols * final_frame.rows * 3, pipes[x][y]);
       }
     }
   }
