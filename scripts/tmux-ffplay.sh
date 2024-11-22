62d743f6a31cebd37e42c450b3f300e131ef8805
Thomas <mast3r@skade.dev>
2024-11-22 09:01:29 +0100

    update tmux script

--- scripts/tmux-ffplay.sh
+++ scripts/tmux-ffplay.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-tmux new-session -d "ffplay udp://localhost:8500 -x 960 -y 540 -autoexit && sleep 10"
-tmux split -h ffplay udp://localhost:8510 -x 960 -y 540 -autoexit
-
-tmux split ffplay udp://localhost:8501 -x 960 -y 540 -autoexit
-tmux select-pane -L
-tmux split ffplay udp://localhost:8511 -x 960 -y 540 -autoexit
+for ((x=0;x<$1;x++)); do
+  for ((y=0;y<$2;y++)); do
+    tmux new-session -d ffplay udp://localhost:85$x$y
+  done
+done
