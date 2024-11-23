f6d78a5e39ca9d7b7045ee9528e78ae30fdc237e
Thomas <mast3r@skade.dev>
2024-11-23 01:10:47 +0100

    revert to rtsp for stability, look into better options in another pr

--- src/Hyperwall.cpp
+++ src/Hyperwall.cpp
@@ -4,24 +4,25 @@
 #include <unordered_map>
 
 const Hyperwall::FFmpeg default_ffmpeg(const int RES_X, const int RES_Y, const int X, const int Y, const int FRAMERATE, const int x, const int y) {
-    Hyperwall::FFmpegBuilder builder;
-    auto HOST = std::vector<std::vector<std::string>>{{"10.42.0.136", "10.42.0.239"}, {"10.42.0.183", "10.42.0.159"}}[y][x];
-    return builder.add("-re")
-      .add("-y")
-      .add("-f", "rawvideo")
-      .add("-vcodec", "rawvideo")
-      .add("-pix_fmt", "bgr24")
-      .add("-s", std::format("{}x{}", RES_X/X, RES_Y/Y))
-      .add("-r", std::to_string(FRAMERATE))
-      .add("-i", "-")
-      .add("-f", "mpegts")
-      .add("-preset", "ultrafast")
-      .add("-s", std::format("{}x{}", RES_X/X, RES_Y/Y))
-      .add("-r", std::to_string(FRAMERATE))
-      .url(std::format("udp://{}:8500", HOST))
-      .build(std::format("ffmpeg-{}-{}.log", x, y));
-  }
-
+  Hyperwall::FFmpegBuilder builder;
+  //auto HOST = std::vector<std::vector<std::string>>{{"10.42.0.136", "10.42.0.239"}, {"10.42.0.183", "10.42.0.159"}}[y][x];
+  auto HOST = "0.0.0.0";
+  return builder.add("-re")
+    .add("-y")
+    .add("-f", "rawvideo")
+    .add("-vcodec", "rawvideo")
+    .add("-pix_fmt", "bgr24")
+    .add("-s", std::format("{}x{}", RES_X/X, RES_Y/Y))
+    .add("-r", std::to_string(FRAMERATE))
+    .add("-i", "-")
+    .add("-f", "mpegts")
+    .add("-preset", "ultrafast")
+    .add("-s", std::format("{}x{}", RES_X/X, RES_Y/Y))
+    .add("-r", std::to_string(FRAMERATE))
+    .add("-f", "rtsp")
+    .url(std::format("rtsp://{}:8554/frame/{}/{}", HOST, x, y))
+    .build(std::format("ffmpeg-{}-{}.log", x, y));
+}
 
 Hyperwall::HyperFrame::HyperFrame(const int x, const int y, std::unordered_map<std::string, std::string> settings, const FFmpeg& ffmpeg) :
   x(x),
@@ -31,7 +32,7 @@ Hyperwall::HyperFrame::HyperFrame(const int x, const int y, std::unordered_map<s
   ffmpeg(ffmpeg),
   RES_X(stoi(settings["RES_X"])),
   RES_Y(stoi(settings["RES_Y"])) {
-  std::cout << "Built frame: [x: " << x << ", y: " << y << ", uri: " << ffmpeg.url << "]" << std::endl;
+  std::cout << "Built HyperFrame: [x: " << x << ", y: " << y << ", uri: " << ffmpeg.url << "]" << std::endl;
 }
 
 Hyperwall::HyperFrame::HyperFrame(const HyperFrame& other) :
@@ -48,7 +49,6 @@ void Hyperwall::HyperFrame::run(const cv::Mat& image) {
   auto end_x = (int)(image.cols * ((1.0+x)/X));
   auto start_y = (int)(image.rows * ((0.0 + y)/Y));
   auto end_y = (int)(image.rows * ((1.0+y)/Y));
-  //std::cout << std::format("{}-{} {}-{}", start_x, end_x, start_y, end_y);
 
   auto sub_image = image(cv::Range(start_y, end_y), cv::Range(start_x, end_x));
   cv::Mat resized_image;
@@ -57,6 +57,7 @@ void Hyperwall::HyperFrame::run(const cv::Mat& image) {
 }
 
 Hyperwall::Hyperwall::Hyperwall(VideoSourceT& source, std::unordered_map<std::string, std::string> settings) : source(source.clone()), X(stoi(settings["X"])), Y(stoi(settings["Y"])) {
+  std::cout << "Generating hyperwall..." << std::endl;
   for(auto x = 0; x < X; x++) {
     frames.insert({x, {}});
     for(auto y = 0; y < Y; y++) {
@@ -75,6 +76,7 @@ Hyperwall::Hyperwall::Hyperwall(VideoSourceT& source, std::unordered_map<std::st
 };
 
 void Hyperwall::Hyperwall::run() {
+  std::cout << "Running Hyperwall" << std::endl;
   while(true) {
     auto image = source->read();
     if (image.rows == 0 || image.cols == 0) {
@@ -86,4 +88,5 @@ void Hyperwall::Hyperwall::run() {
       }
     }
   }
+  std::cout << "Finished hyperwall execution" << std::endl;
 }
