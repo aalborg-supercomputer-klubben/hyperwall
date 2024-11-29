8b7dfeb2eca221ebae0cade054b4ae796ad2c37d
Thomas <mast3r@skade.dev>
2024-11-29 16:31:58 +0100

    network optimization

--- flake.nix
+++ flake.nix
@@ -1,25 +1,29 @@
 {
-  description = "Hyperwall ASCK";
+    description = "Hyperwall ASCK";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShellNoCC {
-      packages = let
-          opencvGtk = pkgs.opencv.override (old : { enableGtk2 = true; });
-      in with pkgs; [
-        opencvGtk
-        ffmpeg
-        cmake
-        spdlog
-        argparse
-        catch2_3
-      ];
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-unstable";
     };
-  };
+
+    outputs = { self, nixpkgs }: {
+        devShells = builtins.mapAttrs (system: _: let
+            pkgs = import nixpkgs { inherit system; };
+        in {
+            default = pkgs.mkShellNoCC {
+                packages = let
+                    opencvGtk = pkgs.opencv.override (old: { enableGtk2 = true; });
+                in with pkgs; [
+                    opencvGtk
+                    ffmpeg
+                    cmake
+                    spdlog
+                    argparse
+                    catch2_3
+                ];
+            };
+        }) {
+            "x86_64-linux" = {};
+            "aarch64-linux" = {};
+        };
+    };
 }
